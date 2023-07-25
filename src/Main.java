5327878dbdf3ebe437fad2eb1736d5d9dc9d73d0
Alvin Alic <alvin.alic@gmail.com>
2023-07-24 22:01:32 -0500

    Add methods for converting the degrees

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        
+    private double fahrenheit;
+    private double celcius;
+    private double conversion;
+
+    public double convertToCelcius(double degrees)
+    {
+        return (degrees - 32) * (double)(5/9);
+    }
+
+    public double converToFahrenheit(double degrees)
+    {
+        return (degrees * 1.8) + 32;
+    }
+    public static void main(String[] args)
+    {
+
     }
 }
